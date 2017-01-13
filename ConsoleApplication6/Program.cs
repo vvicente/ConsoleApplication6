f8e90ed9ec507db996245af3e54141c3e8220f59
Vladir Vicente <vl.vicente@hotmail.com>
2017-01-13 17:19:26 +0000

    Test Commit

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Before using");
+            using (var context = new MusicContext())
+            {
+                 var count = context.Albums.Count();
+                Console.WriteLine(count);
+                context.Albums.Add(new Album() { Price = 9.99m, Title = "Encore" });
+                context.SaveChanges();
+                count = context.Albums.Count();
+                Console.WriteLine(count);
+                var albums = context.Albums.Where(p => p.Title.Contains("Smells")).ToList();
+                Console.WriteLine(albums.Count );
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public Album()
+        {
+            this.AlbumId = 111;
+            this.Title = "Default Album Title";
+            this.Price = 4;
+        }
+    }
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+
+namespace ConsoleApplication6 {
+
+    [TestFixture]
+    public class AlbumTest {
+
+        Album ab;
+        [SetUp]
+        public void Init()
+        {
+            ab = new Album();
+        }
+
+        [Test]
+        public void CreateAlbum()
+        {
+            Album ab = new Album();
+            Assert.AreEqual(111, ab.AlbumId);
+            Assert.AreEqual("Default Album Title", ab.Title);
+        }
+
+        [Test]
+        public void CheckPrice()
+        {
+            Album ab = new ConsoleApplication6.Album();
+            Assert.True(3 < ab.Price);
+        }
+    }
+}
